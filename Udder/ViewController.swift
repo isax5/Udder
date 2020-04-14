ac9f3b14bc8cda646c68322f383d2fd82af1954d
Isaac <isaac.rebolledoleal@gmail.com>
2020-04-14 13:00:12 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Isaac Rebolledo Leal on 14-04-20.
+//  Copyright © 2020 Isaac Rebolledo Leal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
